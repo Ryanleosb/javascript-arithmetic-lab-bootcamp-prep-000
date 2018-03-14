50505f479649eda097c07778ccfabc77dbc632a8
leo bang <ryanleosb@gmail.com>
2018-03-14 03:10:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add (a,b){
+  return a+b;
+}
+function subtract (a,b){
+  return a-b;
+}
+
+function multiplies (a,b) {
+  return a*b;
+}
+
+function divides (a,b){
+  return a/b;
+}
+
+function increment (n){
+  return n+=1;
+}
+function dec (n) {
+  reutnr n-=1;
+}
+
+function makeINT(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  reutrn parseFloat(n);
+}
